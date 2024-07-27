ac71b7cab0314e273c6d980ff94ed98d009a0a91
Hardik Rathod <hardik.r@rafltlabs.com>
2024-07-27 18:06:57 +0530

    feat: simple graphql nestjs setup with basic query

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { UsersModule } from './users/users.module'
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: process.env.NODE_ENV === 'development',
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      //   buildSchemaOptions: {
+      //      numberScalarMode: 'integer',
+      //   },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/users/dto/update-user.input.ts
@@ -0,0 +1,8 @@
+import { Field, InputType, PartialType } from '@nestjs/graphql'
+import { CreateUserInput } from './create-user.input'
+
+@InputType()
+export class UpdateUserInput extends PartialType(CreateUserInput) {
+  @Field(() => String)
+  uid: string
+}
