b6038d070bbdcfd357bb14531518285d64038e1f
Hardik Rathod <hardik.r@rafltlabs.com>
2024-09-12 17:17:22 +0530

    :sparkles: feat(library): Network library which contains generated gql types

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@parkease/network/src/config/apollo'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ApolloProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ApolloProvider>
   )
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,22 @@
+'use client'
+import { useQuery } from '@apollo/client'
+import { CompaniesDocument } from '@parkease/network/src/gql/generated'
 import { add } from '@parkease/sample-lib'
 
 export default function Home() {
-  return <main>Hello {add(343, 3)}</main>
+  const { data, loading } = useQuery(CompaniesDocument)
+
+  return (
+    <main>
+      Hello {add(343, 3)}
+      {/* <div>
+        {data?.companies.map((company) => (
+          <div className="p-4 bg-gray-100 rounded" key={company.id}>
+            <div>{company.displayName}</div>
+            <div>{company.description}</div>
+          </div>
+        ))}
+      </div> */}
+    </main>
+  )
 }
