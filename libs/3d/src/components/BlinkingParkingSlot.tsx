b6f6a8e50c6d0cabb5423b5a8eb45f113593b252
Hardik Rathod <hr8257068@gmail.com>
2024-09-20 14:29:41 +0530

    :sparkles: feat(library): Creating 3d parking animated page

---
+++ libs/3d/src/components/BlinkingParkingSlot.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { yellowColor } from '../util/constants'
+import { Square, SquareProps } from './Square'
+
+interface BlinkingParkingSlotProps extends SquareProps {
+  blinkDuration?: number
+}
+
+export const BlinkingParkingSlot = ({
+  borderColor = yellowColor,
+  blinkDuration = 1000,
+  ...props
+}: BlinkingParkingSlotProps) => {
+  const [isBlinking, setIsBlinking] = useState(true)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsBlinking((prevState) => !prevState)
+    }, blinkDuration)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [blinkDuration])
+
+  if (isBlinking) return null
+
+  return <Square {...props} borderColor={borderColor} />
+}
