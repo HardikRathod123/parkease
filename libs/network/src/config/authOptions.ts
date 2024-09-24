e80b5260d91127be6a109e71e0bb5f428c385ec7
Hardik Rathod <hr8257068@gmail.com>
2024-09-24 13:58:10 +0530

    :bug: fix(library): fixing root domain in nextauth

--- libs/network/src/config/authOptions.ts
+++ libs/network/src/config/authOptions.ts
@@ -15,7 +15,7 @@ const MAX_AGE = 1 * 24 * 60 * 60
 
 const secureCookies = process.env.NEXTAUTH_URL?.startsWith('https://')
 const hostName = new URL(process.env.NEXTAUTH_URL || '').hostname
-const rootDomain = 'karthicktech.com'
+const rootDomain = 'hardik-tech.com'
 
 export const authOptions: NextAuthOptions = {
   // Configure authentication providers
